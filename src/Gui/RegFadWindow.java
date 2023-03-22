0e5531a0adc93daa7772d2cfb897d9140a7eb611
Mikkel <Eaamikklj@students.eaaa.dk>
2023-03-22 11:33:45 +0100

    GUI

--- src/Gui/RegFadWindow.java
+++ src/Gui/RegFadWindow.java
@@ -12,7 +12,7 @@ import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 
-public class regFadWindow extends Stage {
+public class RegFadWindow extends Stage {
 
     private Fad fad;
 
@@ -23,12 +23,12 @@ public class regFadWindow extends Stage {
     private Button okBut, canBut;
 
     private ComboBox<Lager> cbbLager;
-    public regFadWindow(String title, Fad fad) {
+
+    public RegFadWindow(String title) {
         this.initStyle(StageStyle.UTILITY);
         this.initModality(Modality.APPLICATION_MODAL);
         this.setResizable(false);
 
-        this.fad = fad;
 
         this.setTitle(title);
         GridPane pane = new GridPane();
@@ -40,10 +40,6 @@ public class regFadWindow extends Stage {
         this.setHeight(260);
     }
 
-    public regFadWindow(String title) {
-        this(title, null);
-    }
-
     private void initContent(GridPane pane) {
         pane.setPadding(new Insets(10));
         pane.setHgap(10);
@@ -51,81 +47,84 @@ public class regFadWindow extends Stage {
         pane.setGridLinesVisible(false);
 
         lblLev = new Label("Leverandør");
-        pane.add(lblLev,0,0);
+        pane.add(lblLev, 0, 0);
 
         txfLev = new TextField();
-        pane.add(txfLev,1,0);
+        pane.add(txfLev, 1, 0);
 
         lblTidligere = new Label("Tidligere indhold");
-        pane.add(lblTidligere,1,0);
+        pane.add(lblTidligere, 1, 0);
 
         txfTidligere = new TextField();
-        pane.add(txfTidligere,1,1);
+        pane.add(txfTidligere, 1, 1);
 
         lblBrugt = new Label("Antal gange brugt");
-        pane.add(lblBrugt,2,0);
+        pane.add(lblBrugt, 2, 0);
 
         txfBrugt = new TextField();
-        pane.add(txfBrugt,1,2);
+        pane.add(txfBrugt, 1, 2);
 
         lblNr = new Label("Nummer");
-        pane.add(lblNr,3,0);
+        pane.add(lblNr, 3, 0);
 
         txfNr = new TextField();
-        pane.add(txfNr,1,3);
+        pane.add(txfNr, 1, 3);
 
-        lblLiter= new Label("Antal Liter");
-        pane.add(lblNr,4,0);
+        lblLiter = new Label("Antal Liter");
+        pane.add(lblNr, 4, 0);
 
         txfLiter = new TextField();
-        pane.add(txfNr,1,4);
+        pane.add(txfNr, 1, 4);
 
         lblLager = new Label("Lager");
-        pane.add(lblLager,5,0);
+        pane.add(lblLager, 5, 0);
 
         txfLager = new TextField();
-        pane.add(txfLager,1,5);
+        pane.add(txfLager, 1, 5);
 
         okBut = new Button("Ok");
-        pane.add(okBut,0,6);
+        pane.add(okBut, 0, 6);
         okBut.setOnAction(event -> this.okAction());
 
 
         canBut = new Button("Cancel");
-        pane.add(canBut,1,6);
+        pane.add(canBut, 1, 6);
         canBut.setOnAction(event -> this.canAction());
 
         cbbLager = new ComboBox<>();
-        pane.add(cbbLager,1,5);
+        pane.add(cbbLager, 1, 5);
         cbbLager.getItems().addAll(Controller.getLager());
 
 
     }
 
-    private void canAction(){
+    private void canAction() {
         this.hide();
     }
 
-    private void okAction(){
+    private void okAction() {
         String leverandør = txfLev.getText().trim();
         String tidligere = txfTidligere.getText().trim();
-        String brugt = txfBrugt.getText().trim();
-        String nr = txfNr.getText().trim();
-        String liter = txfNr.getText().trim();
+        int brugt = Integer.parseInt(txfBrugt.getText());
+        int nr = Integer.parseInt(txfNr.getText());
+        int liter = Integer.parseInt(txfLiter.getText());
         Lager lager = cbbLager.getSelectionModel().getSelectedItem();
-        if(leverandør.length() == 0 || tidligere.length() == 0 || brugt.length() == 0 || nr.length() == 0 || liter.length() == 0){
+        if (leverandør.length() == 0 || tidligere.length() == 0) {
             Alert dialog = new Alert(Alert.AlertType.INFORMATION);
             dialog.setTitle("Error");
             dialog.setHeaderText("Angiv noget i alle felter");
             dialog.showAndWait();
+            else if (brugt == 0 || nr == 0 || liter == 0) {
+                Alert dialog1 = new Alert(Alert.AlertType.INFORMATION);
+                dialog1.setTitle("Error");
+                dialog1.setHeaderText("Brugt, nummer og liter må ikke være 0");
+                dialog1.showAndWait();
+            }
         }
 
-        //Controller.createFad(leverandør,tidligere,brugt,nr,liter,)
+        //Controller.createFad(leverandør,tidligere,brugt,nr,liter,lager);
 
     }
-
-
-
 
 
 }
