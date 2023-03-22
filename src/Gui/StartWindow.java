8af3782e8c90f6d860c2c49c0f2976793434fc94
Mikkel <Eaamikklj@students.eaaa.dk>
2023-03-22 09:33:46 +0100

    Whisky klasse oprettet

--- src/Gui/StartWindow.java
+++ src/Gui/StartWindow.java
@@ -1,5 +1,7 @@
 package Gui;
 
+import Application.Controller.Controller;
+import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.TabPane;
 import javafx.scene.layout.BorderPane;
