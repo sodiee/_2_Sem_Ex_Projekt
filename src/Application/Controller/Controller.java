8ffbdad83ec2c4a714fa119f77628e3505b13e73
sodiee <mathi.carlsen@gmail.com>
2023-03-22 09:29:53 +0100

    Tre-lags,Fad,Lager,Controller,Storage

--- src/Application/Controller/Controller.java
+++ src/Application/Controller/Controller.java
@@ -5,13 +5,25 @@ import Application.Model.Lager;
 import Storage.Storage;
 
 public class Controller {
-    public void createFad(String leverandør, String tidligereIndhold, int antalGangeBrugt, int nummer, int størrelseLiter, Lager lager) {
+    public static Fad createFad(String leverandør, String tidligereIndhold, int antalGangeBrugt, int nummer, int størrelseLiter, Lager lager) {
         Fad fad = new Fad(leverandør, tidligereIndhold, antalGangeBrugt, nummer, størrelseLiter, lager);
         Storage.addFad(fad);
+        return fad;
     }
 
-    public void createLager(int reoler, int hylder, int pladsPåHylde, String adresse) {
+    public static Lager createLager(int reoler, int hylder, int pladsPåHylde, String adresse) {
         Lager lager = new Lager(reoler, hylder, pladsPåHylde, adresse);
         Storage.addLager(lager);
+        return lager;
+    }
+
+    public static void initStorage() {
+        Lager sønderhøj = createLager(2, 3, 3, "Sønderhøj 30");
+        Lager sørenFrichsVej = createLager(3, 2, 2, "Søren Frichs Vej");
+
+        Fad fad = createFad("Sherry distilleri, Lissabon", "Sherry", 1, 64, 130, sønderhøj);
+        Fad fad1 = createFad("Bourbon distilleri, Texas", "Bourbon", 1, 326, 90, sønderhøj);
+        Fad fad2 = createFad("Sherry distilleri, Madrid", "Sherry", 2, 2, 95, sønderhøj);
+        Fad fad3 = createFad("Rødvin farm, Paris", "Rødvin", 1, 54, 50, sørenFrichsVej);
     }
 }
