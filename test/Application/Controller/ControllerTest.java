83d8383868b4f5a6a79c4fd64b1a1965d3ea566a
sodiee <mathi.carlsen@gmail.com>
2023-03-24 09:14:31 +0100

    Lavet nogle af test

--- test/Application/Controller/ControllerTest.java
+++ test/Application/Controller/ControllerTest.java
@@ -1,5 +1,6 @@
 package Application.Controller;
 
+import Application.Model.Destillat;
 import Application.Model.Fad;
 import Application.Model.Lager;
 import Storage.Storage;
@@ -49,6 +50,32 @@ class ControllerTest {
         assertTrue(actualBoolean);
     }
 
-//@Test
-//void createDestillat()
+    @Test
+    void createDestillat() {
+        //Arrange
+
+        //Act
+        Destillat destillat = Controller.createDestillat("Bingo Dorthe", 50, 20, LocalDate.of(2001, 01, 01), LocalDate.of(2004, 01, 01), "Byg", "Whisky lavet på byg, whiskyen er rød");
+
+        //Assert
+        Boolean actualBoolean = Storage.getDestillatArrayList().contains(destillat);
+        assertTrue(actualBoolean);
+    }
+
+    @Test
+    void createDestillat2() {
+        //Arrange
+
+        //Act
+        Destillat destillat = Controller.createDestillat("Bingo Dorthe", 50, 20, LocalDate.of(2001, 01, 01), LocalDate.of(2004, 01, 01), "Byg", "Whisky lavet på byg, whiskyen er rød");
+
+        //Assert
+        Boolean actualBoolean = Storage.getDestillatArrayList().contains(destillat);
+        assertTrue(actualBoolean);
+
+        Exception forventet = assertThrows(RuntimeException.class, () -> {
+            Controller.createDestillat("Bingo Dorthe", 0, 20, LocalDate.of(2001, 01, 01), LocalDate.of(2004, 01, 01), "Byg", "Whisky lavet på byg, whiskyen er rød");
+        });
+        assertEquals(forventet.getMessage(), "Ugyldig data");
+    }
 }
